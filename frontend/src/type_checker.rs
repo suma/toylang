e710b039fae12b6bc0c23631d0a35eb280355e43
suma <suman.orega.warukatta@gmail.com>
2025-03-15 20:27:28 +0900

    fix check_block

--- frontend/src/type_checker.rs
+++ frontend/src/type_checker.rs
@@ -171,7 +171,7 @@ pub fn check_block(ast: &ExprPool, e: ExprRef, ctx: &mut TypeCheckContext) -> Re
                 };
                 return_types.push(def_ty);
             }
-            return_types.iter().all(|ty| ty == &return_types[0]);
+            return_types.iter().all(|ty| *ty == return_types[0]);
             Ok(return_types[0].clone())
         }
         _ => panic!("check_block: expected block but {:?}", ast.0.get(e.0 as usize).unwrap()),
